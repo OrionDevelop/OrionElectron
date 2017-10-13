4e9d1cf5469f3f4e526e5ef8aac5f9ae45e9d340
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2017-10-13 13:56:59 +0900

    :+1: UI Improvement

--- src/renderer/components/Compose.vue
+++ src/renderer/components/Compose.vue
@@ -3,7 +3,7 @@
     el-card.box-card(:body-style="bodyStyle")
       div.header(slot="header")
         i.el-icon-fa-20.el-icon-fa-envelope-o
-        span New Tweet
+        span New Status
         i.last.el-icon-fa-20.el-icon-fa-times
       .content
         .section
@@ -12,8 +12,8 @@
             el-checkbox-button(v-for="(uuid, index) in accounts.map((w) => w.uuid)" :checked="index === 0" @change="onChange($event, uuid)" :key="uuid")
               circle-image(:src="iconFor(uuid)" :height="48" :width="48")
         .section
-          p Tweet
-          el-input.dff4BcE1A7(type="textarea" v-model="text" :rows="7" v-shortkey="['ctrl', 'enter']" @shortkey.native="onSubmit()" placeholder="What's happening?")
+          p Status
+          el-input.dff4BcE1A7(type="textarea" v-model="text" :rows="5" v-shortkey="['ctrl', 'enter']" @shortkey.native="onSubmit()" placeholder="What's happening?")
           .right.send-button
             el-button(type="primary" :disabled="!canPost" @click="onSubmit") Send
 </template>
