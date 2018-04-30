3c15d28440e2fccf86a8fb09e0e417b7585c3d72
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-05-01 00:20:28 +0900

    :construction: HTML to Pug

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,33 +1,28 @@
-<template>
-  <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
+<template lang="pug">
+#wrapper
+  img#logo(src='~@/assets/logo.png' alt='electron-vue')
+  main
+    .left-side
+      span.title
+        | Welcome to your new project!
+      system-information
+    .right-side
+      .doc
+        .title Getting Started
+        p
+          | electron-vue comes packed with detailed documentation that covers everything from
+          | internal configurations, using the project structure, building your application,
+          | and so much more.
+        button(@click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')") Read the Docs
+        br
+        br
+      .doc
+        .title.alt Other Documentation
+        button.alt(@click="open('https://electron.atom.io/docs/')") Electron
+        button.alt(@click="open('https://vuejs.org/v2/guide/')") Vue.js
 
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
-  </div>
 </template>
+
 
 <script>
 import SystemInformation from "./LandingPage/SystemInformation";
